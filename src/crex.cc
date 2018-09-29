d8677bceb758983a58ba40fd881e7bef02791ddc
Brett Robinson <octo.banana93@gmail.com>
2018-09-28 19:33:13 -0600

    add getters, setters, and helpers

--- src/crex.cc
+++ src/crex.cc
@@ -1,20 +1,68 @@
 #include "crex.hh"
 
 #include <string>
-#include <sstream>
-#include <regex>
 #include <vector>
-#include <unordered_map>
+#include <regex>
 
 namespace OB
 {
 
-  Crex::Crex()
-  {
-  }
+Crex::Crex()
+{
+}
 
-  Crex::~Crex()
-  {
-  }
+Crex::~Crex()
+{
+}
+
+Crex& Crex::regex(std::string regex_)
+{
+  _regex = std::move(regex_);
+  return *this;
+}
+
+std::string Crex::regex()
+{
+  return _regex;
+}
+
+Crex& Crex::text(std::string text_)
+{
+  _text = std::move(text_);
+  return *this;
+}
+
+std::string Crex::text()
+{
+  return _text;
+}
+
+Crex& Crex::options(std::regex_constants::syntax_option_type opts_)
+{
+  _opts = std::move(opts_);
+  return *this;
+}
+
+std::regex_constants::syntax_option_type Crex::options()
+{
+  return _opts;
+}
+
+Crex& Crex::flags(std::regex_constants::match_flag_type flgs_)
+{
+  _flgs = std::move(flgs_);
+  return *this;
+}
+
+std::regex_constants::match_flag_type Crex::flags()
+{
+  return _flgs;
+}
+
+bool Crex::run()
+{
+
+  return true;
+}
 
 } // namespace OB
--- src/crex.hh
+++ src/crex.hh
@@ -2,9 +2,12 @@
 #define OB_CREX_HH
 
 #include <string>
+#include <vector>
+#include <regex>
 
 namespace OB
 {
+
 class Crex
 {
 public:
@@ -12,14 +15,43 @@ public:
   Crex();
   ~Crex();
 
-private:
+  Crex& regex(std::string regex_);
+  std::string regex();
 
-  std::string flgs;
-  std::string rx;
-  std::string str;
-  bool _color {true};
+  Crex& text(std::string text_);
+  std::string text();
+
+  Crex& options(std::regex_constants::syntax_option_type opts_);
+  std::regex_constants::syntax_option_type options();
+
+  Crex& flags(std::regex_constants::match_flag_type flgs_);
+  std::regex_constants::match_flag_type flags();
+
+  bool run();
+
+private:
+  std::string _regex;
+  std::string _text;
+
+  std::regex_constants::syntax_option_type _opts {};
+  std::regex_constants::match_flag_type _flgs {};
+
+  std::vector<std::string> split(std::string str, std::string delim, int match = 0)
+  {
+    std::vector<std::string> vtok;
+    size_t start {0};
+    auto end = str.find(delim);
+    while (end != std::string::npos && match-- > 0) {
+      vtok.emplace_back(str.substr(start, end - start));
+      start = end + delim.length();
+      end = str.find(delim, start);
+    }
+    vtok.emplace_back(str.substr(start, end));
+    return vtok;
+  }
 
 }; // class Crex
+
 } // namespace OB
 
 #endif // OB_CREX_HH
